6c4502bd01068d64fd76929103c3f2bf65e1d4c7
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-01-29 17:04:58 -0300

    Agrega funciones para manejar errores

--- js/validaciones.js
+++ js/validaciones.js
@@ -12,3 +12,53 @@ function validarNumero(numero) {
     return '';
 }
 
+function manejarError(error, $numero) {
+    let noHayError = true
+
+    if (error) {
+        noHayError = false;
+        $numero.classList.add('error');
+
+        if (!comprobarExisteError(error)) {
+            crearError(error);
+        }
+
+    } else {
+        $numero.classList.remove('error');
+    }
+
+    borrarError(error);
+
+    return noHayError;
+}
+
+function crearError(error) {
+    const $error = document.createElement('li');
+    $error.className = 'list-group-item';
+    $error.textContent = error;
+
+    $formulario.querySelector('#errores').appendChild($error);
+}
+
+function borrarError(error) {
+    const $errores = $formulario.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        if ($errores[i].textContent != error) {
+            $errores[i].remove()
+            break;
+        }
+    }
+}
+
+function comprobarExisteError(error) {
+    const $errores = $formulario.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        if (error === $errores[i].textContent) {
+            return true;
+        }
+    }
+    
+    return false;
+}
